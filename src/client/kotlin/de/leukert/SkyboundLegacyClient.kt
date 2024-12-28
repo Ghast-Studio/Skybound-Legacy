e0c1d2d01041e7c80964cd60ac97be4ccb48e1f6
Luca Leukert <derluleu@gmail.com>
2024-12-28 14:26:37 +0100

    Initial commit

---
+++ src/client/kotlin/de/leukert/SkyboundLegacyClient.kt
@@ -0,0 +1,9 @@
+package de.leukert
+
+import net.fabricmc.api.ClientModInitializer
+
+object SkyboundLegacyClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
